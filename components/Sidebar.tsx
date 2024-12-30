19a72e7bd141ff4144aa447ddb0bc7be331cbfda
abawor <abawor200@gmail.com>
2024-12-30 10:13:42 +0000

    Updated styling including changing trash icon to react icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,30 +1,32 @@
 import React from "react"
 import { SidebarProps } from "../types"
 import { FaHeart } from "react-icons/fa";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 export default function Sidebar(props: SidebarProps) {
     const noteElements = props.notes.map((note) => (
         <div key={note.id}>
             <div
-                
                 className={`title ${
                     note.id === props.currentNote.id ? "selected-note" : ""
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
                 <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
-                <button 
-                    className="delete-btn"
-                    onClick={() => props.deleteNote(note.id)}
-                >
-                    <i className="gg-trash trash-icon"></i>
-                </button>
-                <button 
-                    className={note.favourite ? "fav-note" : "fav-btn"}
-                    onClick={() => props.toggleFavourite(note.id)}
-                >
-                    <FaHeart />
-                </button>
+                <div className="btns-div">
+                    <button 
+                        className="delete-btn"
+                        onClick={() => props.deleteNote(note.id)}
+                        >
+                        <FaRegTrashAlt />
+                    </button>
+                    <button 
+                        className={note.favourite ? "fav-note" : "fav-btn"}
+                        onClick={() => props.toggleFavourite(note.id)}
+                        >
+                        <FaHeart />
+                    </button>
+                </div>
             </div>
         </div>
     ))
