81609d3aa86124d6eaea5daf9b10d64c01a44253
abawor <abawor200@gmail.com>
2024-12-31 08:42:51 +0000

    Added first test

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,7 +1,15 @@
-import { it, expect, describe } from 'vitest'
+import React from "react"
+import { it, expect, describe } from "vitest"
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom/vitest"
+import App from "../src/App"
 
-describe('group', () => {
-    it('should', () => {
-        expect(1).toBeTruthy()
+describe("App", () => {
+    it("should render you have no notes if no notes present", () => {
+        render(<App />)
+
+        const heading = screen.getByText("You have no notes")
+
+        expect(heading).toBeInTheDocument()
     })
 })
