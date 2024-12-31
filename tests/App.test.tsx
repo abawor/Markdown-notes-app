cddecb51539b70cdc4584a11dfc1a4a3bf822425
abawor <abawor200@gmail.com>
2024-12-31 08:51:42 +0000

    Added test 2 "render add a new note button"

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -5,11 +5,17 @@ import "@testing-library/jest-dom/vitest"
 import App from "../src/App"
 
 describe("App", () => {
-    it("should render you have no notes if no notes present", () => {
+    beforeEach(() => {
         render(<App />)
-
+    })
+    
+    it("should render you have no notes if no notes present", () => {
         const heading = screen.getByText("You have no notes")
-
         expect(heading).toBeInTheDocument()
+    })
+
+    it("render add a new note button", () => {
+        const button = screen.getByRole("button", { name: /create one now/i })
+        expect(button).toBeInTheDocument()
     })
 })
